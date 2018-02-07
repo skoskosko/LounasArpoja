c30563e495b299111f169378128219c55f641562
Esko Takku <esko.takku@gmail.com>
2018-02-08 00:14:42 +0200

    väli vilkkuvilla jotak ei toimi

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,5 +1,5 @@
 var cooldown = false;
-
+var ArvontaCooldown = false;
 function animateHeightUp(id, height){
 
  var obj_height = document.getElementById(id).offsetHeight;
@@ -28,7 +28,34 @@ function animateHeightDown(id, height){
 }
 
 function RavintolaArvonta(){
-	console.log("arvottu!!!");
+
+	if(!ArvontaCooldown){
+		ArvontaCooldown = true;
+		var nodes = document.getElementsByClassName("item-container")[0].childNodes;
+		console.log(nodes);
+			for(var i = 0; i < 10; i++){
+
+				var nod = nodes[Math.floor(Math.random() * nodes.length)];
+
+				try{
+					if(nod.className.indexOf("menu item") !== -1){
+						//document.getElementById("ItemArea").innerHTML = '';
+						//nod.style = "";
+						
+						//document.getElementById("ItemArea").appendChild(nod);
+						wait(100);
+					}else{
+						i--;
+					}
+				}catch(err){
+					i--;
+				}
+
+			}
+			ArvontaCooldown = false;
+	}
+
+
 
 
 }
@@ -41,33 +68,38 @@ function RavintolaArvonta(){
 	function mouseOver(){
 		if (!cooldown) {
 			animateHeightUp("popup", (window.innerHeight - 100))
-			document.getElementById("popup").innerHTML = '\
-			<div id="ArvontaOtsikko"> Lounas Arvonta !!!</div> \
-			<div id="ArpajaisNappi" onclick="RavintolaArvonta()">ARVO!!!</div>';
+			document.getElementById("ArpajaisNappi").style.display = "block";
 		}
 	}
 
 	function mouseOut(){
 		if (cooldown) {
 			animateHeightDown("popup", (window.innerHeight / 10))
-			document.getElementById("popup").innerHTML = '\
-			<div id="ArvontaOtsikko"> Lounas Arvonta !!!</div>';
+			document.getElementById("ArpajaisNappi").style.display = "none";
 		}
 	}
 
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
+
+
+
+
+
 		clearInterval(readyStateCheckInterval);
 
 		document.getElementById("popup").innerHTML = '\
-		<div id="ArvontaOtsikko"> Lounas Arvonta</div>';
+		<div id="ArvontaOtsikko"> Lounas Arvonta</div>\
+		<div id="ArpajaisNappi">Arvo!<div>\
+		<div id="ItemArea"><div>';
 		document.getElementById("popup").style.display = "block";
+		document.getElementById("ArpajaisNappi").style.display = "none";
 		// document.getElementById("popup").style.background = "none";
 		document.getElementById("popup").style.height = (window.innerHeight / 10) + "px";
 		// document.getElementsByClassName("wrapper")[0].innerHTML = '<div class = "content"> asd </div> <br>'  + oldData;
 		document.getElementById("popup").addEventListener("mouseover", mouseOver);
 		document.getElementsByClassName("wrapper")[0].addEventListener("mouseover", mouseOut);
-
+		document.getElementById("ArpajaisNappi").onclick = RavintolaArvonta;
 
 
 	}
